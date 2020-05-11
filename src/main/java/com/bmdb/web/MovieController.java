da59b62587d9e1ddb6f8a2087d4d26df9d6a1b72
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-05-11 17:30:35 -0400

    added cross orgin

--- src/main/java/com/bmdb/web/MovieController.java
+++ src/main/java/com/bmdb/web/MovieController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import com.bmdb.business.JsonResponse;
 import com.bmdb.business.Movie;
 import com.bmdb.db.MovieRepository;
-
+@CrossOrigin 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
